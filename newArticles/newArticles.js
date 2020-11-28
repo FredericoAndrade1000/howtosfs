63f22ef24a43ed1c549ff9690a59e55fa5b453d6
FredericoAndrade1000 <74203644+FredericoAndrade1000@users.noreply.github.com>
2020-11-28 16:27:31 -0300

    Add files via upload

--- newArticles/newArticles.js
+++ newArticles/newArticles.js
@@ -1,10 +1,19 @@
-var newArticles = ['<div class="colP"><a href="../../articles/tips01/index.html"><h4>3 dicas para começar bem no Spaceflight Simulator</h4></a></div>', '<div class="colP"><a href="../../articles/howtogetorbit/index.html"><h4>Como chegar a órbita no Spaceflight Simulator</h4></a></div>']
+var articleCount = 1
+var newTutorialsArticles = ['<div class="colP"><a href="../../articles/tips01/index.html"><h4>3 dicas para começar bem no Spaceflight Simulator</h4></a></div>', '<div class="colP"><a href="../../articles/howtogetorbit/index.html"><h4>Como chegar a órbita no Spaceflight Simulator</h4></a></div>']
 
-var insertPosition = document.getElementById("newArticles")
-var articlesCount = 1
+var newPartsArticles = ['<div class="colP"><a href="../../articles/capsule-part/index.html"><h4>Cápsula</h4></a></div>', '<div class="colP"><a href="../../articles/hawkEngine-part/index.html"><h4>Hawk Engine</h4></a></div>']
 
-while (articlesCount <= 10 && newArticles[articlesCount - 1] != undefined){
-    insertPosition.insertAdjacentHTML('beforeend', newArticles[articlesCount - 1])
-    console.log(insertPosition)
-    articlesCount++
+newTutorialsArticles.reverse()
+newPartsArticles.reverse()
+var insertPositionTutorials = document.getElementById("newTutorials")
+var insertPositionParts = document.getElementById("newParts")
+
+show(articleCount, newTutorialsArticles, insertPositionTutorials)
+show(articleCount, newPartsArticles, insertPositionParts)
+
+function show(count, news, insertPosition){
+    while (count <= 10 && news[count - 1] != undefined){
+        insertPosition.insertAdjacentHTML('beforeend', news[count - 1])
+        count++
+    }
 }
