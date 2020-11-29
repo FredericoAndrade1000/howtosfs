c484ba7936fca065f85f2e340434c3eebcf37ff9
FredericoAndrade1000 <74203644+FredericoAndrade1000@users.noreply.github.com>
2020-11-29 09:40:47 -0300

    Add files via upload

--- newArticles/newArticles.js
+++ newArticles/newArticles.js
@@ -1,7 +1,7 @@
 var articleCount = 1
 var newTutorialsArticles = ['<div class="colP"><a href="../../articles/tips01/index.html"><h4>3 dicas para começar bem no Spaceflight Simulator</h4></a></div>', '<div class="colP"><a href="../../articles/howtogetorbit/index.html"><h4>Como chegar a órbita no Spaceflight Simulator</h4></a></div>']
 
-var newPartsArticles = ['<div class="colP"><a href="../../articles/capsule-part/index.html"><h4>Cápsula</h4></a></div>', '<div class="colP"><a href="../../articles/hawkEngine-part/index.html"><h4>Hawk Engine</h4></a></div>']
+var newPartsArticles = ['<div class="colP"><a href="../../articles/capsule-part/index.html"><h4>Cápsula</h4></a></div>', '<div class="colP"><a href="../../articles/hawkEngine-part/index.html"><h4>Motor Hawk</h4></a></div>', '<div class="colP"><a href="../../articles/probe-part/index.html"><h4>Controlador remoto</h4></a></div>']
 
 newTutorialsArticles.reverse()
 newPartsArticles.reverse()
