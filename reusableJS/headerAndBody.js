f9e49b171a51d55eb8ebb34c6c54b56a9431aef5
FredericoAndrade1000 <74203644+FredericoAndrade1000@users.noreply.github.com>
2020-12-06 19:43:32 -0300

    Add files via upload

--- reusableJS/headerAndBody.js
+++ reusableJS/headerAndBody.js
@@ -1,14 +1,6 @@
 const headerAndNav =  '<header>'+
                 '<a class="logo" href="https://howtosfs.ml/index.html"><em>HowToSFS</em>.ml</a>'+
-            '</header>'+
-            '<nav>'+
-                '<ul>'+
-                    '<li><a href="https://howtosfs.ml/index.html">HOME</a></li>'+
-                    '<li><a href="https://howtosfs.ml/articlesList/html/index.html">ARTIGOS</a></li>'+
-                    '<li><a href="https://howtosfs.ml/about/html/index.html">SOBRE</a></li>'+
-                    '<li><a href="https://howtosfs.ml/contribute/html/index.html">CONTRIBUA</a></li>'+
-                '</ul>'+
-            '</nav>'
+            '</header>'
 const insertPositionBody = document.getElementById("body")
 add(headerAndNav, insertPositionBody)
 function add(object, insertPosition){
